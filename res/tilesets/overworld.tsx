65ea1981e9b181df3dd4ba96d9d1d9ae9cdb0c98
DrJamgo <drjamgo@hotmail.com>
2019-12-11 16:30:24 +0100

    FEAT: Worldmap beautification

--- res/tilesets/overworld.tsx
+++ res/tilesets/overworld.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.4" name="overworld" tilewidth="16" tileheight="16" tilecount="780" columns="26">
  <image source="../../assets/tilesets/LPC Overworld/LPC_overworld_assembly.png" width="416" height="480"/>
+ <tile id="133" probability="0.2"/>
+ <tile id="134" probability="0.1"/>
+ <tile id="135" probability="0.1"/>
+ <tile id="136" probability="0.2"/>
+ <tile id="137" probability="0.1"/>
+ <tile id="138" probability="0.1"/>
+ <tile id="523" probability="0.2"/>
+ <tile id="524" probability="0.1"/>
+ <tile id="525" probability="0.1"/>
+ <tile id="676" probability="0.1"/>
+ <tile id="677" probability="0.1"/>
+ <tile id="678" probability="0.1"/>
 </tileset>
