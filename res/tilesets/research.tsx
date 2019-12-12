9b333920742577515076476ee12149b79154c83d
DrJamgo <drjamgo@hotmail.com>
2019-12-12 16:42:19 +0100

    FEAT: Add reasearch tiles

---
+++ res/tilesets/research.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="research" tilewidth="32" tileheight="32" tilecount="21" columns="7">
+ <image source="../../assets/tilesets/research.png" width="224" height="96"/>
+ <tile id="1" type="Research"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15" type="Settings"/>
+</tileset>
